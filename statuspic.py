4c8c162332a54d4eddeb63eeb0375df540f29c99
Nico Weber <nicolasweber@gmx.de>
2012-05-28 21:20:58 -0700

    Add GA tracking

--- statuspic.py
+++ statuspic.py
@@ -89,6 +89,17 @@ footer {
   font-size:9pt;
 }
 </style>
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-32179377-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
 </head>
 <body>
 """
@@ -161,12 +172,28 @@ class MainHandler(webapp2.RequestHandler):
 # (it usually does).
 # http://www.google.com/webmasters/+1/button/
 image_html = '''\
+<!doctype html>
+<html>
+<head>
 <style>
 body {
   width: %dpx;
   margin: 20px auto;
 }
 </style>
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-32179377-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+</script>
+</head>
+<body>
 <a href="%s"><img src="%s" width="%d" height="%d"></a>
 <p><g:plusone size="medium" annotation="inline"></g:plusone></p>
 
@@ -177,6 +204,8 @@ body {
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
   })();
 </script>
+</body>
+</html>
 '''
 class ServeImageHandler(webapp2.RequestHandler):
     def get(self, resource):
